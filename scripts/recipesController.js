21341e669443ccbee9bfaf4c164694758e2d1e0f
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-05-27 19:24:26 +0200

    Trying to get the recipes, I will work at my PC. This commit is just for transfer

--- scripts/recipesController.js
+++ scripts/recipesController.js
@@ -4,12 +4,20 @@ const router = express.Router()
 const recipes = require("./recipes.json")
 
 
+
 router.get("/home", (request, response) => {
     response.send((fs.appendFileSync(__dirname + "index.html")))
     }
 )
 router.get("/overview", (request, response) => {
         const titles = Object.keys(recipes);
-        response.json(titles);
+        let german = []
+        for (let y = 0; y < titles.length; y++){
+            for (let i = 0; i < titles[y].length; i++){
+                console.log(recipes.titles[y][i].name)
+                german.push(recipes.titles[y][i].name)
+            }
+        }
+        response.json(german + titles);
 })
 module.exports = router
