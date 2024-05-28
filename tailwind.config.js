18301e7a493a50ba8b881b17af83c365b1ed0443
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-05-28 18:15:50 +0200

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./scripts/overview.html",
+        "./scripts/index.html",
+        "./scripts/**/*.{js,jsx,ts,tsx}",
+        "./scripts/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
