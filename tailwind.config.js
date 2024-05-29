dcdc7940aec0de2eea3fda962ed6d18368680524
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-05-29 09:39:19 +0200

    began from scratch didnt like the template I used, so I deleted all classes and began from scratch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: "jit",
     content: [
         "./scripts/overview.html",
         "./scripts/index.html",
         "./scripts/**/*.{js,jsx,ts,tsx}",
         "./scripts/**/*.{js,jsx,ts,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
